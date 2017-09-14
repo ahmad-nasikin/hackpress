8853a80c392e5376a8562f644b5c41e166620d2a
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-09-14 14:07:12 +0700

    Add model user dan article

--- server/app.js
+++ server/app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://livecoding:livecoding@cluster0-shard-00-00-mtaex.mongodb.net:27017,cluster0-shard-00-01-mtaex.mongodb.net:27017,cluster0-shard-00-02-mtaex.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var article = require('./routes/article')
 
 var app = express();
 
@@ -17,6 +21,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/article', article)
 
 module.exports = app;
